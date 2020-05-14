587d213e33bfce2f845a59c267b7943b0acf0e12
sajojohnmnb <sajo.john@millar-bryce.com>
2020-05-14 22:16:35 +0100

    animation using keyframes and offset

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -4,6 +4,7 @@ import {
   transition,
   style,
   animate,
+  keyframes,
 } from '@angular/animations';
 
 export let slide = trigger('slide', [
@@ -13,7 +14,13 @@ export let slide = trigger('slide', [
   ]),
   transition(':leave', [
     style({ transform: 'translateX(-10px)' }),
-    animate('0.5s ease-in', style({ transform: 'translateX(-100%)' })),
+    animate(
+      '0.5s ease-in',
+      keyframes([
+        style({ offset: 0.2, opacity: 1, transform: 'translateX(20px)' }),
+        style({ offset: 1, opacity: 0, transform: 'translateX(-100%)' }),
+      ])
+    ),
   ]),
 ]);
 
